602e09e39034625b61ebe72fcf1347a3a9bacf1c
John V McCarthy <jvmnpm@gmail.com>
2016-01-19 17:17:08 -0700

    Attempt to reproduce sidenav blank rendering issue on IE11 without success.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ var app = angular.module('angular-material-scratchpad', [
   'ngMaterial',
 ]);
 
-app.controller('AppController', function() {
- 
+app.controller('AppController', function($mdSidenav) {
+  var vm = this;
+  vm.toggleRight = toggleRight;
+  
+  //////////////
+  
+  function toggleRight() {
+    $mdSidenav('right').toggle();
+  }
 });
